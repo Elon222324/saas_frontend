7ff6ecf74b0a64d5c4113b341b73b17a1cae29eb
Elon222324 <ptmu000@gmail.com>
2025-06-17 18:58:18 +0700

    Разбил Категории товаров на модули

--- src/pages/Sites/SiteSettings/Products/index.jsx
+++ src/pages/Sites/SiteSettings/Products/index.jsx
@@ -3,7 +3,7 @@ import { useRef, useState } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import CategoryList from './components/CategoryList/CategoryList'
-import ProductsList from './components/ProductsList'
+import ProductsList from './components/ProductsList/ProductsList'
 
 export default function Products() {
   const queryClientRef = useRef(new QueryClient())
